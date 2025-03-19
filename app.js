e9a970225a87e876bee0f867bc9634c55dd471a6
iam5026TeslaCoilsFTC <huntertot09@gmail.com>
2025-03-19 17:43:39 -0600

    Fixed spelling Errors

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
-let inro = doccument.querySelector('.intro');
+let intro = document.querySelector('.intro');
 let logo = document.querySelector('.logoa');
-let logospan = document.querySelectorAll('.logo');
+let logospan = document.querySelectorAll('.intro span');
 
-window.addEventListener('DOMContentLoaded', ()=> {
-
-    setTimeout(()=>{
-
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
         logospan.forEach((span, idx) => {
-            setTimeout(()=> {
+            setTimeout(() => {
                 span.classList.add('active');
-            }, (idx + 1) * 400)
+            }, (idx + 1) * 400);
         });
 
-        setTimeout(()=> {
+        setTimeout(() => {
             logospan.forEach((span, idx) => {
-                setTimeout(()=> {
+                setTimeout(() => {
                     span.classList.remove('active');
                     span.classList.add('fade');
-                }, (idx + 1) * 50)
-            })
+                }, (idx + 1) * 50);
+            });
         }, 2000);
 
-        setTimeout(()=> {
-            inro.style.top = '-100vh';
-        }, 2300)
-
-    })
-})
+        setTimeout(() => {
+            intro.style.top = '-100vh';
+        }, 2300);
+    });
+});
