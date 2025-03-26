9c1170d926104878f53317d8ba20719fe04572e4
iam5026TeslaCoilsFTC <huntertot09@gmail.com>
2025-03-26 17:49:14 -0600

    Changed Logo Animation to Gif that hides once the page is done loading

---
+++ preload.js
@@ -0,0 +1,5 @@
+// Wait for the page to fully load
+window.addEventListener("load", function () {
+    document.getElementById("preloader").style.display = "none"; // Hide preloader
+    document.getElementById("content").style.display = "block"; // Show content
+});
